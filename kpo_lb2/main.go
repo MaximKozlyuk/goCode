4f68016a8a6ca20514374d61442fd318a278f815
MaximKozlyuk <maxim.kozlyuk@yandex.ru>
2018-10-21 17:04:47 +0400

    kpo lab 2 plot added

--- kpo_lb2/main.go
+++ kpo_lb2/main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"math"
+	"math/rand"
+	"time"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 /*
@@ -32,7 +39,8 @@ func main() {
 	//Ni := make([]int, 7,7)
 	//Ni[0] = 4; Ni[1] = 4; Ni[2] = 3 ;Ni[3] = 3; Ni[4] = 2; Ni[5] = 1; Ni[6] = 0
 
-	// test variant
+	// test variant // todo init all values {}
+	// todo make Ni arr float64, recode with links and draw init data
 	Ni := make([]int, 6, 6)
 	Ni[0] = 3
 	Ni[1] = 4
@@ -50,12 +58,19 @@ func main() {
 	// ∆ni расчетное :
 	fmt.Println()
 	var errorsLeft float64
+	arrY := make([]float64, 0, 0)
+	arrX := make([]float64, 0, 0)
 	for i := 0; i < len(Ni); i++ {
 		fmt.Print("∆n")
 		fmt.Println(i, " = ", No*math.Pow(math.E, -K*float64(i+1))*K)
 		errorsLeft += No * math.Pow(math.E, -K*float64(i+1)) * K
+
+		arrY = append(arrY, No*math.Pow(math.E, -K*float64(i+1))*K)
+		arrX = append(arrX, float64(i))
 	}
 	fmt.Println("Errors left: ", No-errorsLeft)
+
+	renderPlot(&arrX, &arrY)
 
 }
 
@@ -120,4 +135,58 @@ func calc_No(k, Dt float64, ni []int) float64 {
 		denominator += math.Pow(math.E, -2*k*float64(i+1)) * float64(i+1)
 	}
 	return numerator / (k * Dt * denominator)
+}
+
+func renderPlot(arrX, arrY *[]float64) {
+	// Get values
+	values := packArrayForPlot(arrX, arrY)
+
+	// Create a new plot, set its title and
+	// axis labels.
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+	p.Title.Text = "Модель джевинского-Моранды"
+	p.X.Label.Text = "∆t"
+	p.Y.Label.Text = "n"
+	// Draw a grid behind the data
+	p.Add(plotter.NewGrid())
+
+	// Make a scatter plotter and set its style.
+	s, err := plotter.NewLine(values)
+	if err != nil {
+		panic(err)
+	}
+	s.LineStyle.Color = color.RGBA{R: 255, B: 128, A: 255}
+
+	// Add the plotters to the plot, with a legend
+	// entry for each
+	p.Add(s)
+	p.Legend.Add("Решение моджели", s)
+
+	// Save the plot to a PNG file.
+	if err := p.Save(12*vg.Inch, 12*vg.Inch, "points.png"); err != nil {
+		panic(err)
+	}
+}
+
+func packArrayForPlot(arrX, arrY *[]float64) plotter.XYs {
+	pts := make(plotter.XYs, len(*arrX))
+	for i := range *arrX {
+		pts[i].X = (*arrX)[i]
+		pts[i].Y = (*arrY)[i]
+	}
+	return pts
+}
+
+// randomPoints returns some random x, y points for plot
+func randomPoints(n int) plotter.XYs {
+	pts := make(plotter.XYs, n)
+	for i := range pts {
+		rand.Seed(time.Now().UnixNano())
+		pts[i].X = rand.Float64() * float64(rand.Intn(100))
+		pts[i].Y = rand.Float64() * float64(rand.Intn(100))
+	}
+	return pts
 }
